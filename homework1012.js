0c2dc2c6d42fb994738503436f5ca264a1453bbf
Christopher Rescorla <cgrescorla@gmail.com>
2016-10-10 10:10:33 -0600

    adding homework assignment for 10/12

---
+++ homework1012.js
@@ -0,0 +1,10 @@
+/**
+ * Created by GodaiYuusaku on 10/10/16.
+ */
+
+/**
+ * Create a Javascript program that continuously asks a user for 5 numbers (look at prompt in Javascript) that are comma separated
+ * The program should show the same numbers but sorted.
+ * - Cannot use array.sort()
+ * - Must use an array, a loop, a conditional
+ **/
