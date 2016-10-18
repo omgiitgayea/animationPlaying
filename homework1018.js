7bc866a83ceb9803fbbf8cb114877f249dc16931
Christopher Rescorla <cgrescorla@gmail.com>
2016-10-18 07:58:44 -0600

    homework assignment due 10/24/2016

---
+++ homework1018.js
@@ -0,0 +1,10 @@
+/**
+ * Created by GodaiYuusaku on 10/18/16.
+ */
+/** Create an async function that reverses each string in an array
+    - cannot use string.reverse()
+    - must return a promise
+    - must set a timeout to simulate async behavior
+    - must render the resulting array after it has been modified
+
+    go to https://github.com/MountainlandWEB/TeachingMaterials to find in class promise code */
