d2b73d2f83cfb1a1a24e8ae94f4123f9794b799e
Christopher Rescorla <cgrescorla@gmail.com>
2016-10-18 17:08:23 -0600

    promise homework

--- homework1018.js
+++ homework1018.js
@@ -8,3 +8,65 @@
     - must render the resulting array after it has been modified
 
     go to https://github.com/MountainlandWEB/TeachingMaterials to find in class promise code */
+var desiredGoal = "Make a ham sandwich for me";
+var startingArray = desiredGoal.split(" ");
+var endingArray = [];
+var spell = document.getElementById("zatannaSpell");
+var disclaimer = document.getElementById("disclaimer");
+
+function asyncZatannaSpell(stringArray)
+{
+    promise = new Promise(execute);
+    return promise;
+
+    function execute(resolve, reject) {
+        setTimeout(function ()
+        {
+            if (stringArray.length <= 0)
+            {
+                reject("How can she say a spell with no words!?")
+            }
+            zatanna(stringArray);
+            resolve();
+        }, 3000);
+    }
+}
+
+var zatannaPromise = asyncZatannaSpell(startingArray);
+zatannaPromise.then(success, failure);
+
+function success ()
+{
+    spell.innerHTML = printWords(endingArray) + " *";
+    spell.style.display = "inline-block";
+    TweenMax.from(spell, 2, {opacity: 0, ease:Circ.easeInOut, y: 0});
+    setTimeout(function () {
+        disclaimer.innerHTML = "* Note: Zatana says '" + printWords(startingArray) + "' instead of 'Make me a ham sandwich' because magic is very literal...";
+        TweenMax.from(disclaimer, 1, {opacity: 0, ease:Power2.easeInOut, y: 0});
+    }, 3000);
+}
+function failure ()
+{
+    document.getElementById("zatannaSpell").innerHTML = "How can she say a spell with no words!?";
+}
+
+function zatanna(stringArray) {
+    for (var j = 0; j < stringArray.length; j++) {
+        var backwards = "";
+        for (var i = (stringArray[j].length - 1); i >= 0; i--) {
+            backwards += stringArray[j][i];
+        }
+        endingArray.push(backwards);
+    }
+}
+
+function printWords(wordsArray)
+{
+    var printString = "";
+    for (var i = 0; i < (wordsArray.length - 1); i++)
+    {
+        printString += wordsArray[i] + " ";
+    }
+    printString += wordsArray[wordsArray.length - 1] + "!";
+    return printString;
+}
